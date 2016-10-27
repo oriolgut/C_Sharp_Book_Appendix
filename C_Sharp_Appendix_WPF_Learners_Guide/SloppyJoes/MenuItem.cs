b10986f09fe71b49f290eb94954843c427cc95c2
OriolG <download.spam.net@gmail.com>
2016-10-27 16:12:34 +0200

    finished Project SloppyJoes

---
+++ C_Sharp_Appendix_WPF_Learners_Guide/SloppyJoes/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SloppyJoes
+{
+    public class MenuItem
+    {
+        public MenuItem(string meat, string condiment, string bread)
+        {
+            Meat = meat;
+            Bread = bread;
+            Bread = bread;
+        }
+
+        public string Meat { get; set; }
+        public string Condiment { get; set; }
+        public string Bread { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Meat} with {Condiment} on {Bread}.";
+        }
+    }
+}
---
+++ C_Sharp_Appendix_WPF_Learners_Guide/SloppyJoes/MenuMaker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SloppyJoes
+{
+    class MenuMaker : INotifyPropertyChanged
+    {
+        private Random _random = new Random();
+        private List<string> _meats = new List<string>() { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami" };
+        private List<string> _condiments = new List<string>() { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+        private List<string> _breads = new List<string>() { "rye", "white", "wheat", "pumpernickel", "italian bread", "a roll" };
+
+        public MenuMaker()
+        {
+            Menu = new ObservableCollection<MenuItem>();
+            NumberOfItems = 10;
+            UpdateMenu();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<MenuItem> Menu { get; private set; }
+        public DateTime GeneratedDate { get; set; }
+        public int NumberOfItems { get; set; }
+
+        public void UpdateMenu()
+        {
+            Menu.Clear();
+            for (int i = 0; i < NumberOfItems; i++)
+            {
+                Menu.Add(CreateMenuItem());
+            }
+
+            GeneratedDate = DateTime.Now;
+
+            OnPropertyChanged(nameof(GeneratedDate));
+        }
+
+        private MenuItem CreateMenuItem()
+        {
+            string randomMeat = _meats[_random.Next(_meats.Count)];
+            string randomCondiment = _condiments[_random.Next(_condiments.Count)];
+            string randomBread = _breads[_random.Next(_breads.Count)];
+            return new MenuItem(randomMeat, randomCondiment, randomBread);
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
