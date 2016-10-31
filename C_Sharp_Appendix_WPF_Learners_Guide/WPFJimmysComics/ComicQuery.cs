31a1f7dcc315cd2cb4ed37f6d25cd4576cf29684
OriolG <download.spam.net@gmail.com>
2016-10-31 15:45:55 +0100

    created and finished WPFJimmysComics Project

---
+++ C_Sharp_Appendix_WPF_Learners_Guide/WPFJimmysComics/ComicQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WPFJimmysComics
+{
+    class ComicQuery
+    {
+        public string Title { get; private set; }
+        public string Subtitle { get; private set; }
+        public string Description { get; private set; }
+        public BitmapImage Image { get; private set; }
+        
+        public ComicQuery(string title, string subtitle, string description, BitmapImage image)
+        {
+            Title = title;
+            Subtitle = subtitle;
+            Description = description;
+            Image = image;
+        }
+    }
+}
---
+++ C_Sharp_Appendix_WPF_Learners_Guide/WPFJimmysComics/PriceRange.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFJimmysComics
+{
+    enum PriceRange
+    {
+        Cheap,
+        Midrange,
+        Expensive
+    }
+}
